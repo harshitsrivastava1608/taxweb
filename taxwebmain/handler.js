4dd0d662e7066ea92e504aa09d653efe7cce27f0
harshitsrivastava1608 <harshit.srivastava1608@gmail.com>
2024-08-01 14:27:44 +0530

    first commit

---
+++ taxwebmain/handler.js
@@ -0,0 +1,9 @@
+
+export const hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v4! Your function executed successfully!",
+    }),
+  };
+};
